68b003828747ccb84e0e76754e57591309bebd99
AJ1 <austinjohnson3d@gmail.com>
2015-12-28 06:09:51 -0500

    Update 12/28

--- PloppableRCI/PloppableRCI.cs
+++ PloppableRCI/PloppableRCI.cs
@@ -1,168 +1,26 @@
-using ColossalFramework;
-using ColossalFramework.Globalization;
-using ColossalFramework.Math;
-using ColossalFramework.Plugins;
-using System;
-using UnityEngine;
 using ICities;
-using ColossalFramework.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace PloppableAI
 {
-	public class PloppableExperiment : IUserMod 
+	public class PloppableRCI : IUserMod
 	{
-
-		public string Name 
+		public string Name
 		{
-			get { return "PloppableRCI"; }
-		}
-
-		public string Description 
-		{
-			get { return "Adds Ploppable RCI"; }
-		}
-	}
-
-	//This is my current attempts at making the UI button with some code copied from various mods. Still learning how it all works. 
-
-	public class PloppableTool : ToolBase
-	{
-		UIButton mainButton;
-		BuildingInfo BuildingIm;
-		UIPanel BuildingPanel;
-
-		UITabstrip Tabs;
-		bool m_active;
-		UIScrollablePanel ScrollPanel;
-		UIButton ResTab;
-	
-		UIButton BBut;
-	
-		protected override void Awake()
-		{
-			//this.m_dataLock = new object();
-			this.m_active = false;
-			base.Awake();
-		}
-
-		public void InitGui (System.Collections.Generic.List<string> BNames) {
-
-			mainButton = UIView.GetAView ().FindUIComponent<UIButton> ("MarqueeBulldozer");
-
-			if (mainButton == null) {
-				
-				var RCIButton = UIView.GetAView ().FindUIComponent<UIMultiStateButton> ("BulldozerButton");
-
-				mainButton = RCIButton.parent.AddUIComponent<UIButton> ();
-				mainButton.name = "RCIButton";
-				mainButton.size = new Vector2 (36, 36);
-				mainButton.normalBgSprite = "ZoningOptionMarquee";
-				mainButton.focusedFgSprite = "ToolbarIconGroup6Focused";
-				mainButton.hoveredFgSprite = "ToolbarIconGroup6Hovered";
-				mainButton.relativePosition = new Vector2 (
-					RCIButton.relativePosition.x + RCIButton.width / 2.0f - mainButton.width - RCIButton.width,
-					RCIButton.relativePosition.y + RCIButton.height / 2.0f - mainButton.height / 2.0f
-				);
-				
-
-				mainButton.eventClick += buttonClicked;
-
-				BuildingPanel = UIView.GetAView ().FindUIComponent ("TSContainer").AddUIComponent<UIPanel> ();
-
-				BuildingPanel.backgroundSprite = "SubcategoriesPanel";
-				BuildingPanel.isVisible = false;
-				BuildingPanel.name = "BuildingPanel";
-				BuildingPanel.size = new Vector2 (859, 109);
-				BuildingPanel.relativePosition = new Vector2 (0, 0);
-
-				ScrollPanel = UIView.GetAView ().FindUIComponent ("BuildingPanel").AddUIComponent<UIScrollablePanel> ();
-				ScrollPanel.size = new Vector2 (763, 109);
-				ScrollPanel.relativePosition = new Vector2 (50, 0);
-
-				Tabs = UIView.GetAView ().FindUIComponent ("BuildingPanel").AddUIComponent<UITabstrip> ();
-				Tabs.size = new Vector2 (832, 25);
-				Tabs.relativePosition = new Vector2 (13, -25);
-
-				ResTab = Tabs.AddUIComponent<UIButton> ();
-				ResTab.size = new Vector2 (58, 25);
-				ResTab.relativePosition = new Vector2 (2, 0);
-				ResTab.normalBgSprite = "SubBarButtonBase";
-				ResTab.pressedBgSprite = "SubBarButtonBasePressed";
-				ResTab.hoveredBgSprite = "SubBarButtonBaseHovered";
-				ResTab.focusedBgSprite = "SubBarButtonBaseFocused";
-
-				ResTab.normalFgSprite = "ZoningResidentialHigh";
-				ResTab.pressedFgSprite = "ZoningResidentialHighPressed";
-				ResTab.hoveredFgSprite = "ZoningResidentialHighHovered";
-				ResTab.tabStrip = true;
-
-
-				string[] Bdnames = new string[]{};
-				Bdnames = BNames.ToArray ();
-				int Offset = 0;
-				int counter = 0;
-
-				foreach (string test in Bdnames) {
-
-					Debug.Log (Convert.ToString (test));
-					BuildingIm = PrefabCollection<BuildingInfo>.FindLoaded (test);
-					this.GenBButton (BuildingIm, Offset);
-					Offset = Offset + 109;
-					counter = counter + 1;
-				}
-			}
-
-		}
-		
-
-		void GenBButton (BuildingInfo BuildingIm, int Offset){
-			
-			BBut = new UIButton ();
-			BBut = ScrollPanel.AddUIComponent<UIButton> ();
-			BBut.name = name;
-			BBut.size = new Vector2 (109, 100);
-			BBut.atlas = BuildingIm.m_Atlas;
-			BBut.normalFgSprite = BuildingIm.m_Thumbnail;
-			BBut.focusedFgSprite = BuildingIm.m_Thumbnail + "Focused";
-			BBut.hoveredFgSprite = BuildingIm.m_Thumbnail + "Hovered";
-			BBut.pressedFgSprite = BuildingIm.m_Thumbnail + "Pressed";
-			BBut.disabledFgSprite = BuildingIm.m_Thumbnail + "Disabled";
-			BBut.isEnabled = enabled;
-			BBut.relativePosition = new Vector2 (Offset,5);
-			BBut.eventClick += (sender, e) => BuildingBClicked(sender, e, BuildingIm);
-
-			 
-
-			Debug.Log (Convert.ToString (name));
-			
-		}
-
-		void BuildingBClicked(UIComponent component, UIMouseEventParameter eventParam, BuildingInfo Binf ){
-
-			BuildingTool buildingTool = ToolsModifierControl.SetTool<BuildingTool>();
+			get
 			{
-				buildingTool.m_prefab = Binf;
-				buildingTool.m_relocate = 0;
+				return "Ploppable RICO";
 			}
 		}
-
-		void buttonClicked(UIComponent component, UIMouseEventParameter eventParam)
+		public string Description
 		{
-			this.enabled = true;
-			BuildingPanel.isVisible = true;
+			get
+			{
+				return "Allows Plopping of Residential Buildings";
+			}
 		}
-		protected override void OnDisable()
-		{
-			if (BuildingPanel != null)
-				BuildingPanel.isVisible = false;
-			base.OnDisable();
-		}
-		protected override void OnEnable()
-		{
-			UIView.GetAView().FindUIComponent<UITabstrip>("MainToolstrip").selectedIndex = -1;
-			base.OnEnable();
-		}
-
-
 	}
 }
---
+++ PloppableRCI/ServiceInfoWindow.cs
@@ -0,0 +1,133 @@
+﻿using ColossalFramework.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace PloppableAI
+{
+
+	public class ServiceInfoWindow : MonoBehaviour
+	{
+		UILabel info;
+		UILabel label1;
+		FieldInfo baseSub;
+		FieldInfo bSub;
+		UITabstrip Tabs;
+		UIButton ResTab;
+		InstanceID BID = new InstanceID();
+	
+		Building data;
+		int num;
+
+		//CityServiceWorldInfoPanel tester = UIView.GetAView ().FindUIComponent ("(Library) CityServiceWorldInfoPanel");
+
+
+		CityServiceWorldInfoPanel m_servicePanel;
+		public CityServiceWorldInfoPanel servicePanel
+		{
+			get { return m_servicePanel; }
+			set
+			{
+				var stats = value.Find<UIPanel>("StatsPanel");
+				info = stats.Find<UILabel>("Info");
+				label1 = stats.AddUIComponent<UILabel>();
+				label1.color = info.color;
+				label1.textColor = info.textColor;
+				label1.textScale = info.textScale;
+				label1.relativePosition = new Vector3(0, info.height + info.relativePosition.y - 40);
+				label1.size = new Vector2(230, 84);
+				label1.font = info.font;
+
+				Tabs = UIView.GetAView ().FindUIComponent ("(Library) CityServiceWorldInfoPanel").AddUIComponent<UITabstrip> ();
+				Tabs.size = new Vector2 (432, 25);
+				Tabs.relativePosition = new Vector2 (13, -25);
+				DrawTabs ("ZoningResidentialHigh", 5, "SResidential", 30534);
+				ResTab.isVisible = false;
+		
+				m_servicePanel = value;
+			}}
+
+		int lastSelected;
+
+			
+		public void Update()
+		{
+			
+
+
+			if (servicePanel == null) {
+				return;
+			}
+	
+
+			var buildingId = GetParentInstanceId ().Building;
+			if (this.enabled && info.isVisible && BuildingManager.instance != null && ((SimulationManager.instance.m_currentFrameIndex & 15u) == 15u || lastSelected != buildingId)) {
+				Debug.Log ("running");
+				lastSelected = buildingId;
+
+				Building data = BuildingManager.instance.m_buildings.m_buffer [buildingId];
+
+				BID.Building = data.m_subBuilding;
+
+				Building SubB = BuildingManager.instance.m_buildings.m_buffer [data.m_subBuilding];
+
+				if (SubB.Info.m_buildingAI is PloppableResidential) {
+					//label1.text = "";
+					ResTab.isVisible = true;
+					ResTab.eventClick += (sender, e) => SelectSub(sender, e, data.m_subBuilding);
+				} else {
+					ResTab.isVisible = false;
+				}
+
+			}
+		}
+		private InstanceID GetParentInstanceId()
+		{
+			if (baseSub == null)
+			{
+				baseSub = m_servicePanel.GetType().GetField("m_InstanceID", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			}
+			return (InstanceID)baseSub.GetValue(m_servicePanel);
+		}
+		public void DrawTabs(string Sprite, int offset, string name, ushort ID){
+
+			ResTab = new UIButton ();
+			ResTab = Tabs.AddUIComponent<UIButton> ();
+			ResTab.size = new Vector2 (58, 25);
+			ResTab.relativePosition = new Vector2 (offset, 0);
+			ResTab.atlas = UIView.GetAView ().FindUIComponent<UIButton> ("CommercialLow").atlas;
+			ResTab.normalBgSprite = "SubBarButtonBase";
+			ResTab.pressedBgSprite = "SubBarButtonBasePressed";
+			ResTab.hoveredBgSprite = "SubBarButtonBaseHovered";
+			ResTab.focusedBgSprite = "SubBarButtonBaseFocused";
+
+
+
+			//ResTab.normalFgSprite = Sprite;
+			ResTab.pressedFgSprite = Sprite + "Pressed";
+			ResTab.hoveredFgSprite = Sprite + "Hovered";
+			ResTab.focusedFgSprite = Sprite + "Focused";
+			//ResTab.tabStrip = true;
+		}
+
+
+			
+	
+		private void SelectSub(UIComponent component, UIMouseEventParameter eventParam, ushort ID){
+			
+			label1.text = ID.ToString() ;
+			BID.Building = ID;
+			DefaultTool.OpenWorldInfoPanel (BID, new Vector2 (0, 0));
+		
+			//WorldInfoPanel.Show<CityServiceWorldInfoPanel> (new Vector2 (200, 200), BID);
+		}
+
+	}
+
+}
+	
+
